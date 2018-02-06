dc613d3ab6c0a425b37267f22c1dec2cf2cec737
Steve Lacy <me@slacy.me>
2018-02-06 11:11:51 -0700

    docker run

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-docker service create \
-  --restart-condition=none \
-  --detach=false \
+docker run \
   --name aurora \
   -e POSTGRES_URL=${POSTGRES_URL} \
   -e POSTGRES_REPLICAS=${POSTGRES_REPLICAS} \
